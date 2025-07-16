b585066c7bcc70da83ff940dce6dfa081d05c3e1
Srykimsour <soursour3001@gmail.com>
2025-07-16 21:13:54 +0700

    protectedroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext"; // Import the AuthProvider
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Dashboard from "./pages/Dashboard";
@@ -7,23 +7,54 @@ import Courses from "./pages/Courses";
 import Teachers from "./pages/Teachers";
 import Students from "./pages/Students";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute"; 
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/teachers" element={<Teachers />} />
-      <Route path="/students" element={<Students />} />
+
+      {/* Protected routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/courses"
+        element={
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/teachers"
+        element={
+          <ProtectedRoute>
+            <Teachers />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/students"
+        element={
+          <ProtectedRoute>
+            <Students />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
 
 export default function App() {
   return (
-    <AuthProvider> {/* Wrap everything with AuthProvider */}
+    <AuthProvider>
       <BrowserRouter>
         <Navbar />
         <AppRoutes />
